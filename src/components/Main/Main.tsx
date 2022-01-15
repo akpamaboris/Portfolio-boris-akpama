20ba6d360230d4bffcaad10102919839bb3211b6
Zeliwipin <zeliwipin@gmail.com>
2022-01-15 23:17:48 +0100

    correction on my portfolio

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,16 +1,15 @@
 export const Main = () => {
   return (
-    <section className="relative py-20 2xl:py-40 bg-gray-800 overflow-hidden">
-      <div className="absolute h-40 w-40 bg-blue-500 bottom-0 right-0 -mr-20 -mb-20 rounded-full"></div>
+    <section className="relative py-20 overflow-hidden bg-gray-800 2xl:py-40">
       <div className="container px-4 mx-auto">
         <div>
           <div className="flex flex-wrap -mx-6 lg:-mx-8">
-            <div className="w-full md:w-1/2 lg:w-1/4 px-6 lg:px-8 mb-20 lg:mb-0">
-              <span className="flex mb-10 justify-center items-center w-full h-20 bg-blue-500 rounded-lg">
+            <div className="w-full px-6 mb-20 md:w-1/2 lg:w-1/4 lg:px-8 lg:mb-0">
+              <span className="flex items-center justify-center w-full h-20 mb-10 bg-blue-500 rounded-lg">
                 <div className="text-white">Projet 1</div>
               </span>
               <div className="pb-px mb-12 bg-gray-500"></div>
-              <h3 className="mt-12 mb-8 text-lg font-bold font-heading text-center text-white">
+              <h3 className="mt-12 mb-8 text-lg font-bold text-center text-white font-heading">
                 Backups
               </h3>
               <p className="text-lg text-center text-gray-200">
